3a8331776b7310ca22ecdf0e23f1631b8d8412b4
Kilisio Veronicah <kilisionicah@gmail.com>
2023-06-07 17:23:48 +0300

    First Commit

---
+++ Static_Files_ASP.NET Core/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Static_Files_ASP.NET_Core.Models;
+using System.Diagnostics;
+
+namespace Static_Files_ASP.NET_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
